b63da650465e42e6a8ab61f978dd9f19b571f717
noaione <noaione0809@gmail.com>
2021-09-03 16:05:51 +0800

    utils: create selectFirst

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ export type NoneType = null | undefined;
 export function isNone(value: any): value is NoneType {
     return value === undefined || value === null;
 }
+
+export function selectFirst(values: string | string[]): string {
+    if (Array.isArray(values)) {
+        return values[0];
+    }
+    return values;
+}
