ce6abf50517e5600cc43d2311a4e9caeade9a937
noaione <noaione0809@gmail.com>
2021-09-03 14:38:13 +0800

    typing: update

--- custom.d.ts
+++ custom.d.ts
@@ -1,8 +1,19 @@
+import type { S3Client } from "@aws-sdk/client-s3";
+
 declare global {
     namespace NodeJS {
         // Extend process.env typing
         interface ProcessEnv {
             VERCEL_GIT_COMMIT_SHA?: string;
+            S3_ENDPOINT?: string;
+            S3_REGION?: string;
+            S3_BUCKET?: string;
+            S3_ACCESS_KEY_ID?: string;
+            S3_SECRET_ACCESS_KEY?: string;
+        }
+
+        interface Global {
+            s3?: S3Client;
         }
     }
 }
