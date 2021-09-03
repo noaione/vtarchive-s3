a7d4a0e6569e2395db2bb0c49e15514a7fab6efe
noaione <noaione0809@gmail.com>
2021-09-03 23:23:25 +0800

    Viewer: add filename guessing

--- components/Viewer.tsx
+++ components/Viewer.tsx
@@ -2,12 +2,14 @@ import { VTObject, VTPath } from "@/lib/s3";
 import React from "react";
 import Link from "next/link";
 
+import mime from "mime-types";
+
 import Breadcrumbs from "./Breadcrumbs";
 import LoadingContainer from "./LoadContainer";
 import NoContent from "./NoContent";
 
 import Icon from "@mdi/react";
-import { mdiFile, mdiFolder } from "@mdi/js";
+import { mdiFile, mdiFileDocument, mdiFileImage, mdiFileMusic, mdiFileVideo, mdiFolder } from "@mdi/js";
 
 interface ViewerProps {
     folders: VTPath[];
@@ -41,6 +43,27 @@ function FolderRender(props: VTPath) {
     );
 }
 
+function determineIcon(filename: string) {
+    const mimeType = mime.lookup(filename);
+    if (!mimeType) {
+        return mdiFile;
+    }
+
+    if (mimeType.startsWith("video/")) {
+        return mdiFileVideo;
+    }
+    if (mimeType.startsWith("image/")) {
+        return mdiFileImage;
+    }
+    if (mimeType.startsWith("audio/")) {
+        return mdiFileMusic;
+    }
+    if (mimeType.startsWith("text/")) {
+        return mdiFileDocument;
+    }
+    return mdiFile;
+}
+
 interface SimpleObject {
     prefix: string;
     file: string;
@@ -54,7 +77,7 @@ function FilesRender(props: SimpleObject) {
             <a>
                 <div className="flex flex-row px-4 py-3 bg-gray-600 rounded-md transition hover:bg-gray-500 cursor-pointer shadow-md">
                     <div className="flex mr-1.5">
-                        <Icon path={mdiFile} size={1} className="text-blue-300" />
+                        <Icon path={determineIcon(realName)} size={1} className="text-blue-300" />
                     </div>
                     <td className="overflow-x-hidden">
                         <div
